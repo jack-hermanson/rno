69a480e1eb294da2a068e92646dd7aeb6895f96c
Jack Hermanson <jack.hermanson@live.com>
2025-10-15 10:12:08 -0600

    ledger add cols

---
+++ application/modules/finances/ledger_item_category_enum.py
@@ -0,0 +1,7 @@
+from enum import IntEnum
+
+
+class LedgerItemCategoryEnum(IntEnum):
+    MEMBERSHIP = 1
+    DONATION = 2
+    WEBSITE_EMAIL_SUBSCRIPTION = 3
--- application/modules/finances/models.py
+++ application/modules/finances/models.py
@@ -6,6 +6,7 @@ from sqlalchemy import Date, DateTime, ForeignKey, Integer, Numeric, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from application import db
+from application.modules.finances.ledger_item_category_enum import LedgerItemCategoryEnum
 from application.modules.finances.ledger_item_type_enum import LedgerItemTypeEnum
 from application.utils.date_time import utcnow
 
@@ -27,6 +28,13 @@ class LedgerItem(db.Model):
         default=utcnow,
     )
 
+    # Protected notes that shouldn't be shown to the public, for personal details like name/address
+    private_notes: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    # Hardcoded category enum
+    category: Mapped[LedgerItemCategoryEnum] = mapped_column(Integer, nullable=True)
+
+    # Foreign key
     created_by_account_id: Mapped[int] = mapped_column(ForeignKey("account.account_id"), nullable=False)
 
     # Relationship back to Account
